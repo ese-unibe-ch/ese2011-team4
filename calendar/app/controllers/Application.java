556804a3dacec0eb69d5f3ef7b2b4f09b69b1310
Joel Krebs <joel.krebs@students.unibe.ch>
2011-10-02 21:30:42 +0200

    Done the first successful css-styling

--- calendar/app/controllers/Application.java
+++ calendar/app/controllers/Application.java
@@ -3,14 +3,14 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
-import java.util.*;
+import java.util.List;
 
 import models.*;
 
 public class Application extends Controller {
-
     public static void index() {
-        render();
+    	List<Calendar> calendars = Calendar.all().fetch();
+    	List<User> users = User.all().fetch();
+        render(calendars, users);
     }
-
 }
