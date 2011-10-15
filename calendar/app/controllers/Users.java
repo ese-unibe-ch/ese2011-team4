4daa74ab875353aa898f1634402be5fd2b07338c
Michael Baertschi <michu.baertschi@students.unibe.ch>
2011-10-15 12:12:47 +0200

    added a simple users page

--- calendar/app/controllers/Users.java
+++ calendar/app/controllers/Users.java
@@ -1,9 +1,27 @@
 package controllers;
 
+import java.util.List;
+
+import models.Calendar;
+import models.User;
 import play.mvc.Controller;
 
 public class Users extends Controller {
 	public static void index() {
-		render();
+		User connectedUser = User.find("email", Security.connected()).first();
+		if(connectedUser != null){
+			Users.display(connectedUser.id);
+		}
+		else {
+	    	render();
+		}
+	}
+	
+	public static void display(Long userId){
+		User connectedUser = User.find("email", Security.connected()).first();
+		User user = User.findById(userId);
+		List<User> users = User.all().fetch();
+		List<Calendar> calendars = Calendar.find("owner", user).fetch();
+		render(users, connectedUser, calendars);
 	}
 }
