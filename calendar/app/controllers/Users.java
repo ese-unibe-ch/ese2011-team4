c34507d54a23d04dcde1aff082a8ff47f19a5571
S Kumar <simpal.kumar@gmail.com>
2011-11-14 22:41:15 +0100

    Validate error in user profile..not able to figure out why it gives in updating

--- calendar/app/controllers/Users.java
+++ calendar/app/controllers/Users.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.persistence.Query;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import models.*;
 import play.db.jpa.JPA;
@@ -16,6 +18,7 @@ import play.mvc.With;
 
 @With(Secure.class)
 public class Users extends Controller {
+	private static DateTimeFormatter format = DateTimeFormat.forPattern("dd.MM.yyyy");
 	public static void index() {
 		List<User> users = User.all().fetch();
 		User connectedUser = User.find("email", Security.connected()).first();
@@ -31,26 +34,46 @@ public class Users extends Controller {
 							String fullname,
 							String nickname,
 							String gender,
-							DateTime birthday,
-							Location loc,
-							String telephone) {
-				User userprof=User.findById(userId);
- 
-				assert userprof != null;
-
-				userprof.fullname=fullname;
-				userprof.setNickName(nickname);
-				userprof.setAddress(loc);
-				userprof.setBirthday(birthday);
-				userprof.setTelephone(telephone);
-				userprof.setGender(gender);
-				show(userId);
+							boolean visiblegender,
+							String birthday,
+							String birthmonth,
+							String birthyear,
+							boolean visiblebirthday,
+							Long locationId,
+							boolean visibleaddress,
+							String telephone,
+							boolean visibletelephone,
+							String descriptionUser
+							) {
+				User connectedUser=User.findById(userId);
+				assert connectedUser != null;
+				Location location = Location.findById(locationId);
+				connectedUser.setAddress(location);
+				connectedUser.setBirthday(format.parseDateTime(birthday+ "."+birthmonth+"."+birthyear));
+				connectedUser.setAddressVisibility(visibleaddress);
+				connectedUser.setDescripton(descriptionUser);
+				connectedUser.setGender(gender);
+				connectedUser.setNickName(nickname);
+				connectedUser.fullname=fullname;
+				connectedUser.setTelephone(telephone);
+				connectedUser.setBirthdayVisibility(visiblebirthday);
+				connectedUser.setTelephoneVisibility(visibletelephone);
+				connectedUser.setGenderVisibility(visiblegender);
+				if(connectedUser.validateAndSave()) {
+		        	show(userId);
+		    	} else {
+		     		for(play.data.validation.Error e : Validation.errors())
+		    			Logger.error(e.message());
+		    		params.flash();
+		            validation.keep();
+		            edit(userId);
+		    	}
+				
 	}
 
 	public static void show(Long id) {
-    	User connectedUser = User.find("byEmail", Security.connected()).first();
-    	List<User> users=User.findAll();
-    	render(connectedUser,users);
+    	User connectedUser = User.findById(id);
+    	render(connectedUser);
     }
 	public static void addFavorite(Long id, Long userId){
 		User connectedUser = User.findById(id);
