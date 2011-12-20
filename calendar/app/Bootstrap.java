b29f1159e9587e50efc66d47ba187f0c9e8d9071
Roger Stebler <roger.stebler@students.ethz.ch>
2011-12-20 16:05:58 +0100

    fixups
    - delete database before loading initial-data.yml. should fix an exception which occurred sometimes while running tests.
    - updated functional test.

--- calendar/app/Bootstrap.java
+++ calendar/app/Bootstrap.java
@@ -18,6 +18,7 @@ public class Bootstrap extends Job {
     public void doJob() {
         // Check if the database is empty and play is running in DEV mode
         if(Play.mode == Play.Mode.DEV && User.count() == 0) {
+        	Fixtures.deleteDatabase();
         	Logger.info("Loading intial data...");
             Fixtures.loadModels("initial-data.yml");
             Logger.info("Loaded the following data:");
--- calendar/test/ApplicationTest.java
+++ calendar/test/ApplicationTest.java
@@ -6,19 +6,11 @@ import play.mvc.Http.*;
 import models.*;
 
 public class ApplicationTest extends FunctionalTest {
-//    @Test
-//    public void testThatIndexPageWorks() {
-//        Response response = GET("/");
-//        assertIsOk(response);
-//        assertContentType("text/html", response);
-//        assertCharset(play.Play.defaultWebEncoding, response);
-//    }
-
-
     @Test
-    public void testThatIndexRedirectsToLogin() {
+    public void testThatIndexPageWorks() {
         Response response = GET("/");
-        assertStatus(302, response);
-        assertHeaderEquals("Location", "/login", response);
+        assertIsOk(response);
+        assertContentType("text/html", response);
+        assertCharset(play.Play.defaultWebEncoding, response);
     }
 }
